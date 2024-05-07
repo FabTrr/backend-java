5b0af313c3408bab92c082941dcba7877e54dc3a
FabTrr <f.torre@outlook.es>
2024-05-07 00:59:50 -0300

    Proyectos de Backend Java

--- PatronFacade/src/test/java/SistemaBusquedaVueloTest.java
+++ PatronFacade/src/test/java/SistemaBusquedaVueloTest.java
@@ -7,13 +7,13 @@ public class SistemaBusquedaVueloTest {
     public void testBuscarVuelo() {
         SistemaBusquedaVuelo sistema = new SistemaBusquedaVuelo();
 
-        // Supongamos que queremos buscar vuelos de Madrid a París para el 2024-05-10 al 2024-05-17
+        // Supongamos que queremos buscar vuelos de Madrid a Paris para el 2024-05-10 al 2024-05-17
         String fechaSalida = "2024-05-10";
         String fechaRegreso = "2024-05-17";
         String origen = "Madrid";
-        String destino = "París";
+        String destino = "Paris";
 
-        // Ejecutamos el metodo de búsqueda de vuelo
+        // Ejecutamos el metodo de busqueda de vuelo
         sistema.buscarVuelo(fechaSalida, fechaRegreso, origen, destino);
 
     }
